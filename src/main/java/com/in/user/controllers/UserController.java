96a0660b7f50169c4a9b7607f825d7ba8a5dce37
savitamandlekar <mandlekarsavita@gmail.com>
2024-04-19 00:02:26 +0530

    Done CircuitBreakers, RateLimiters and Retry

--- src/main/java/com/in/user/controllers/UserController.java
+++ src/main/java/com/in/user/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.in.user.controllers;
 
 import com.in.user.entities.User;
 import com.in.user.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +24,29 @@ public class UserController {
         User user1=service.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
-
+    int retryCount=1;
     @GetMapping("/{userId}")
+    @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+    // @Retry(name="ratingHotelService",fallbackMethod = "ratingHotelFallback")
+    //@RateLimiter(name="userRatelimiter",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+        System.out.println("retryCount:"+retryCount);
+        retryCount++;
         User user=service.getUser(userId);
         return  ResponseEntity.ok(user);
+    }
+
+
+    public ResponseEntity<User> ratingHotelFallback(String userId,Exception ex){
+ //System.out.println("Fallback is executed because service is down : "+ex.getMessage());
+
+        User user= User.builder().
+         email("dummy@gmail.com").
+         name("Dummy").
+         about("This is user created dummy some services is down").
+         userId("12345").
+         build();
+ return new ResponseEntity<>(user,HttpStatus.OK);
     }
 
     @GetMapping
@@ -33,5 +54,6 @@ public class UserController {
         List<User> allUser = service.getAllUser();
         return ResponseEntity.ok(allUser);
     }
+
 
 }
--- src/main/java/com/in/user/entities/User.java
+++ src/main/java/com/in/user/entities/User.java
@@ -11,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 @Table(name = "micro_users")
 public class User {
 
